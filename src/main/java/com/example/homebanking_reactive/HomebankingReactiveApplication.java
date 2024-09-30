80812883a2c9e801d5aa9dc82dc48abe530577c7
DanielRodado <d4nielrodado@gmail.com>
2024-09-30 18:21:09 -0500

    first commit

---
+++ src/main/java/com/example/homebanking_reactive/HomebankingReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.homebanking_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebankingReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingReactiveApplication.class, args);
+	}
+
+}
