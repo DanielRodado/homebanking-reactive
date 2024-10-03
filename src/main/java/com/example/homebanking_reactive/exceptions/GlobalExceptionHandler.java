a9128e8e20737aed7c223f26cd719688bee92dd4
DanielRodado <d4nielrodado@gmail.com>
2024-10-03 16:27:28 -0500

    Nuevas excepciones

--- src/main/java/com/example/homebanking_reactive/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/homebanking_reactive/exceptions/GlobalExceptionHandler.java
@@ -8,9 +8,19 @@ import reactor.core.publisher.Mono;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ValidationException.class)
+    public Mono<ResponseEntity<String>> handleValidationException(ValidationException ex) {
+        return Mono.just(ResponseEntity.badRequest().body(ex.getMessage()));
+    }
+
     @ExceptionHandler(ClientNotFoundException.class)
     public Mono<ResponseEntity<String>> handleClientNotFoundException(ClientNotFoundException ex) {
         return Mono.just(ResponseEntity.status(404).body(ex.getMessage()));
+    }
+
+    @ExceptionHandler(ClientEmailAlreadyExistsException.class)
+    public Mono<ResponseEntity<String>> handleClientEmailAlreadyExistsException(ClientEmailAlreadyExistsException ex) {
+        return Mono.just(ResponseEntity.status(403).body(ex.getMessage()));
     }
 
 }
